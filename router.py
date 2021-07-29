4d8cea5860edbdba1e0a9c0b4325e41c2d2abb38
João Prates <jvdprates@hotmail.com>
2021-07-28 22:44:13 -0300

    Feat:  Função update quase lá

--- router.py
+++ router.py
@@ -1,7 +1,8 @@
 import socket as skt
 import argparse as ap
 import networkx as nx
-import sys
+import threading
+import time
 import json
 
 # Constantes do programa
@@ -11,7 +12,8 @@ PORT = 55151
 parser = ap.ArgumentParser(
     description='Trabalho pratico 2 - Bruno Oliveira, João Prates')
 parser.add_argument('addr', help='Endereço de ip do roteador')
-parser.add_argument('period', help='Período entre envio de mensagens de update (π)')
+parser.add_argument(
+    'period', help='Período entre envio de mensagens de update (π)')
 parser.add_argument("-s", "--startup_commands",
                     help='O nome de um arquivo .txt contendo comandos de teclado para adicionar links')
 args = parser.parse_args()
@@ -35,14 +37,14 @@ def printError(title, message="", note="Erro"):
     print("❗", title)
     if not message == "":
         print("[", note, "]:", message)
-        
+
 # Printa um sucesso
 def printSuccess(title, message="", note="Sucesso"):
     print("✅", title)
     if not message == "":
         print("[", note, "]:", message)
 
-# Inicializa o programa e faz o bind ao endereço    
+# Inicializa o programa e faz o bind ao endereço
 printError("Programa iniciado!")
 socket = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
 socket.bind((ADDR, PORT))
@@ -51,23 +53,76 @@ printSuccess("Roteador vinculado ao endereço {}/{}".format(ADDR, PORT))
 # Verifica se há comandos de startup
 if STARTUP:
     printSuccess("Arquivos recebido", "{}".format(STARTUP), "Nome do arquivo")
-    
-#Variavel local que guarda endereços, tabela de roteamento
-routes = []
-class Address:
-    def __init__(self, address, weight):
-        self.addr = address
-        self.weight = weight
-    def printAddress(self):
-        print("{ address: " + self.addr + ", weight: " + self.weight + " }")
 
+# Variavel local que guarda endereços, tabela de roteamento
+routes = []
+
+# Variavel local que guarda uma tabela de endereços recebidos
+routeTable = {}
+
+# Imprime uma lista de endereços
 def printAddressList(list):
     print("Lista: \n[")
     for i in list:
-        i.printAddress()
+        print(i)
     print("]")
-        
-# Espera o comando do usuario
+
+# Função que retorna vetor de rota-distancia
+def getRouteDistances():
+    obj = {}
+    for i in routes:
+        obj[i["addr"]] = i["weight"]
+    return obj
+
+# Função que cria um JSON data
+def createJSON(obj, type):
+    if type == "update":
+        return json.dumps({
+            "type": "update",
+            "source": ADDR,
+            "destination": obj["destination"],
+            "distances": getRouteDistances()
+        })
+    elif type == "trace":
+        return json.dumps({
+            "type": "trace",
+            "source": ADDR,
+            "destination": obj["destination"],
+            "hops": obj["hops"],
+        })
+    elif type == "data":
+        return json.dumps({
+            "type": "data",
+            "source": ADDR,
+            "destination": obj["destination"],
+            "payload": obj["payload"],
+        })
+
+# Função que periodicamente manda update para todos as rotas
+def updateTh():
+    while True:
+        for i in routes:
+            data = createJSON({"destination": i["addr"]},"update")
+            socket.sendto(bytes(data, "UTF-8"), (i["addr"], PORT))
+        time.sleep(int(PI))
+threading.Thread(target = updateTh).start()
+
+# Função que recebe as mensagens de outros roteadores
+def receiveTh():
+    while True:
+        data = socket.recv(1024)
+        message = data.decode("UTF-8")
+        printSuccess("Recebi uma mensagem!", message)
+        if message['type'] == "update":
+            print("Update!")
+            routeTable[message["source"]] = message["distances"]
+        elif message['type'] == "trace":
+            print("Trace!")
+        elif message['type'] == "data":
+            print("Data!")     
+threading.Thread(target = receiveTh).start()
+
+# Espera o comando do usuario 
 print("Para fechar, digite 'q', 'quit' ou ctrl+c")
 command, args = get_input()
 # printSuccess("Input recebido", "{} - {}".format(command, args), "Inputs:")
@@ -76,14 +131,15 @@ while command not in ["q", "quit"]:
     if len(args) > 1:
         weight = args[1]
     if command == 'add':
-        if not any(x for x in routes if x.addr == address):
-            routes.append(Address(address, weight))
+        if not any(x for x in routes if x["addr"] == address):
+            routes.append({"addr": address, "weight": weight})
             printSuccess("Endereço adicionado!")
             printAddressList(routes)
         else:
             printError("Endereço já existe na lista!")
     elif command == 'del':
-        index = next((x for x, item in enumerate(routes) if item.addr == address), None)
+        index = next((x for x, item in enumerate(routes)
+                     if item["addr"] == address), None)
         if index != None:
             routes.pop(index)
             printSuccess("Endereço removido!")
@@ -93,6 +149,7 @@ while command not in ["q", "quit"]:
     elif command == 'trace':
         printSuccess("Trace!")
     else:
-        printError("Comando desconhecido - {}".format(command), "Tente 'add', 'del', 'trace' ou 'quit'")
+        printError("Comando desconhecido - {}".format(command),
+                   "Tente 'add', 'del', 'trace' ou 'quit'")
     command, args = get_input()
 printSuccess("Finalizando programa...")
