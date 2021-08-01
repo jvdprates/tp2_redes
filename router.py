20e89552ff340e7a09f038f0c5fd9b3f7406c955
João Prates <jvdprates@hotmail.com>
2021-08-01 17:01:18 -0300

    Feat: Função de recebimento de data e trace implementada

--- router.py
+++ router.py
@@ -21,7 +21,8 @@ args = parser.parse_args()
 # ./router.py <ADDR> <PERIOD> [STARTUP]
 # python router.py 127.0.1.0 10
 # add 127.0.1.1 1
-# send 127.0.1.2 data
+# send 127.0.1.3 DOMINGO
+# trace 127.0.1.3
 
 # Argumentos
 ADDR = args.addr
@@ -65,7 +66,7 @@ routeTable = {}
 
 # Imprime uma lista de endereços
 def printAddressList(list):
-    print("Lista: \n[")
+    print("Lista de vizinhos: \n[")
     for i in list:
         print(i)
     print("]")
@@ -82,21 +83,21 @@ def createJSON(obj, type):
     if type == "update":
         return json.dumps({
             "type": "update",
-            "source": ADDR,
+            "source": obj["source"],
             "destination": obj["destination"],
             "distances": getRouteDistances()
         })
     elif type == "trace":
         return json.dumps({
             "type": "trace",
-            "source": ADDR,
+            "source": obj["source"],
             "destination": obj["destination"],
             "hops": obj["hops"],
         })
     elif type == "data":
         return json.dumps({
             "type": "data",
-            "source": ADDR,
+            "source": obj["source"],
             "destination": obj["destination"],
             "payload": obj["payload"],
         })
@@ -105,7 +106,7 @@ def createJSON(obj, type):
 def updateTh():
     while True:
         for i in routes:
-            data = createJSON({"destination": i["addr"]}, "update")
+            data = createJSON({"destination": i["addr"], "source": ADDR}, "update")
             socket.sendto(bytes(data, "UTF-8"), (i["addr"], PORT))
         time.sleep(int(PI))
 threading.Thread(target=updateTh, daemon=True).start()
@@ -115,35 +116,57 @@ def receiveTh():
     while True:
         data = socket.recv(1024)
         message = json.loads(data.decode("UTF-8"))
+        
         if message['type'] == "update":
-            print("Update!")
             routeTable[message["source"]] = message["distances"]
+            
         elif message['type'] == "trace":
-            print("Trace!")
+            destination = message["destination"]
+            source = message["source"]
+            hops = message["hops"]
+            if destination != ADDR:
+                distance, shortest = getShortestFromSelf(destination)
+                if shortest and distance > 0:
+                    sendTrace(shortest, destination, source, hops)
+                else:
+                    printError("Destino não encontrado", "Não foi possível traçar uma rota para o destino escolhido pela mensagem a ser encaminhada")
+            else:
+                message["hops"].append(ADDR)
+                printSuccess("Trace recebido!", message["hops"], "Hops:")
+                distance, shortest = getShortestFromSelf(source)
+                if shortest and distance > 0:
+                    sendPayload(shortest, source, destination, message)
+                else:
+                    printError("Destino não encontrado", "Não foi possível traçar uma rota para o destino escolhido pela mensagem a ser encaminhada")
+                
         elif message['type'] == "data":
-            print("Data!")
-            printSuccess("Mensagem:", message)
+            destination = message["destination"]
+            source = message["source"]
+            payload = message["payload"]
+            if destination != ADDR: 
+                distance, shortest = getShortestFromSelf(destination)
+                if shortest and distance > 0:
+                    sendPayload(shortest, destination, source, payload)
+                else:
+                    printError("Destino não encontrado", "Não foi possível traçar uma rota para o destino escolhido pela mensagem a ser encaminhada")
+            else:
+                printSuccess("Mensagem recebida!", message, "Mensagem:")
 threading.Thread(target=receiveTh, daemon=True).start()
 
 # Função que envia um data a um endereço
-def sendPayload(destination, payload):
-    data = createJSON({"destination": destination, "payload": payload}, "data")
-    socket.sendto(bytes(data, "UTF-8"), (destination, PORT))
-
-# Retorna o peso se o endereço passado for vizinho ao roteador, retorna None se não
-def searchNearby(destination):
-    directLink = next((item for item in routes if item["addr"] == destination), None)
-    if directLink != None:
-        printSuccess("directLink", directLink)
-        return directLink["weight"]
-    printError("Achou não")
-    return None
+def sendPayload(shortest, destination, source, payload):
+    data = createJSON({"destination": destination, "payload": payload, "source": source}, "data")
+    socket.sendto(bytes(data, "UTF-8"), (shortest, PORT))
     
+# Função que envia um trace a um endereço
+def sendTrace(shortest, destination, source, hops):
+    hops.append(ADDR)
+    data = createJSON({"destination": destination, "hops": hops, "source": source}, "trace")
+    socket.sendto(bytes(data, "UTF-8"), (shortest, PORT))
+
 # Busca o caminho mais curto com algorítmo de dijsktra
 def getShortestFromSelf(destination):
     G = nx.Graph()
-    printSuccess("routeTable", routeTable)
-    printSuccess("routes", routes)
     for route in routes:
         G.add_edge(ADDR, route["addr"], weight = float(route["weight"]))
     for top in routeTable:
@@ -151,7 +174,18 @@ def getShortestFromSelf(destination):
             for sub in routeTable[top]:
                 wei = routeTable[top][sub]
                 G.add_edge(top, sub, weight = float(wei))
-    return single_source_dijkstra(G, ADDR, destination)
+
+    obj = single_source_dijkstra(G, ADDR, destination)
+    distance, shortest = obj
+    printSuccess("Distância calculada: {}".format(distance))
+    printSuccess("Caminho:", shortest)
+    if distance > 0:
+        return (distance, shortest[1])
+    elif distance == 0:
+        return (distance, shortest[0])
+    else:
+        printError("Caminho não encontrado", "Calculo de menor distância não deu certo")
+        return (-1, "erro")
 
 # Espera o comando do usuario
 print("Para fechar, digite 'q', 'quit' ou ctrl+c")
@@ -177,10 +211,22 @@ while command not in ["q", "quit"]:
             printError("Endereço não existe na lista!")
     elif command == 'trace':
         printSuccess("Trace!")
+        distance, shortest = getShortestFromSelf(address)
+        if shortest and distance > 0:
+            sendTrace(shortest, address, ADDR, [])
+        elif distance == 0:
+            printError("Próprio endereço", "Você enviou uma mensagem para o próprio roteador")
+        else:
+            printError("Destino não encontrado", "Não foi possível traçar uma rota para o destino escolhido")
     elif command == 'send':
         payload = args[1:]
-        printSuccess("Vizinhos:", searchNearby(address))
-        printSuccess("Menor distância:", getShortestFromSelf(address))
+        distance, shortest = getShortestFromSelf(address)
+        if shortest and distance > 0:
+            sendPayload(shortest, address, ADDR, payload)
+        elif distance == 0:
+            printError("Próprio endereço", "Você enviou uma mensagem para o próprio roteador")
+        else:
+            printError("Destino não encontrado", "Não foi possível traçar uma rota para o destino escolhido")
     else:
         printError("Comando desconhecido - {}".format(command),
                    "Tente 'add', 'del', 'trace', 'send' ou 'quit'")
